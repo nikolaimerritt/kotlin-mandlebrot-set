32689db1967e559407159e15db1b21f8300e78cb
Nikolai Merritt <nikolaimerritt20@gmail.com>
2022-12-26 12:38:10 +0000

    tidied

--- App.kt
+++ App.kt
@@ -1,14 +1,18 @@
 import javax.swing.JFrame
 
-public fun main() {
-	val frame = JFrame("manky brod set")
-	frame.setSize(1920, 1080)
-	frame.isVisible = true
-	frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+class App {
 
-	val panel = Panel()
-	frame.add(panel)
-	frame.addKeyListener(Input(panel))
-	panel.startAnimation()
+	public fun main() {
+		val frame = JFrame("manky brod set")
+		frame.setSize(1920, 1080)
+		frame.isVisible = true
+		frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+
+		val panel = Panel()
+		frame.add(panel)
+		frame.addKeyListener(Input(panel))
+		panel.startAnimation()
+	}
+
 }
 
