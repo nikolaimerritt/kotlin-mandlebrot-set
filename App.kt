d8011de510f7098580a13d1c32b77e12019979b9
Nikolai Merritt <nikolaimerritt20@gmail.com>
2023-05-13 09:02:26 +0100

    Update App.kt

--- App.kt
+++ App.kt
@@ -3,7 +3,7 @@ import javax.swing.JFrame
 class App {
 
 	public fun main() {
-		val frame = JFrame("manky brod set")
+		val frame = JFrame("Mandlebrot set")
 		frame.setSize(1920, 1080)
 		frame.isVisible = true
 		frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
